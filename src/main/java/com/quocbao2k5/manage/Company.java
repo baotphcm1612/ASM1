31c8a7cf19d0c43bb6993390e088ab706a257edc
baotphcm <quocbao2k5xtt@gmail.com>
2024-01-13 12:46:05 +0700

    update

---
+++ src/main/java/com/quocbao2k5/manage/Company.java
@@ -0,0 +1,106 @@
+package com.quocbao2k5.manage;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import com.quocbao2k5.employee.*;
+
+public class Company {
+    private List<Employee> employees = new ArrayList<>();
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+    
+    public void clearList() {
+        if(employees != null && !employees.isEmpty())
+            this.employees.clear();
+    }
+    
+    public void addEmployee(int type) {
+        switch (type) {
+            case 1:
+                System.out.println("Add employee");
+                employees.add(new Employee().setInfo());
+                break;
+            case 2:
+                System.out.println("Add marketing employee");
+                employees.add(new Marketing().setInfo());
+                break;
+            case 3:
+                System.out.println("Add manager");
+                employees.add(new Manager().setInfo());
+                break;
+            default:
+                break;
+        }
+    }
+
+    public Employee findEmployee(String id) {
+        if(employees != null && !employees.isEmpty()) {
+            for(Employee temp : employees) {
+                if(temp.getId().equals(id)) {
+                    return temp;
+                }
+            }
+        }
+        return null;
+    }
+
+    public void filterEmployeeBySalary(double salary) {
+        if(employees != null && !employees.isEmpty()) {
+            boolean isFound = false;
+            for(Employee temp : employees) {
+                if(temp.getSalary() == salary) {
+                    temp.showInfo();
+                    isFound = true;
+                }
+            }
+            if(!isFound) {
+                System.out.println("Employee not found!");
+            }
+        }
+        else {
+            System.out.println("List is empty!");
+        }
+    }
+
+    public void sortByName() {
+        if(employees != null && !employees.isEmpty()) {
+            Collections.sort(employees,Comparator.comparing(Employee::getName));
+            System.out.println("Done!");
+        }
+        else {
+            System.out.println("List is empty!");
+        }
+    }
+
+    public void sortBySalary() {
+        if(employees != null && !employees.isEmpty()) {
+            Collections.sort(employees,Comparator.comparing(Employee::getSalary).reversed());
+        }
+        else {
+            System.out.println("List is empty!");
+        }
+    }
+
+    public void deleteEmployee(String id) {
+        if(employees != null && !employees.isEmpty()) {
+            if(employees.removeIf(temp -> temp.getId().equals(id))) {
+                System.out.println("Delete success!");
+            }
+            else {
+                System.out.println("Employee not found!");
+            }
+        }
+        else {
+            System.out.println("List is empty!");
+        }
+    }
+
+    public void updateInfo(String id) {
+        Employee temp = findEmployee(id);
+        temp.updateInfo();
+    }
+}
---
+++ target/run.bat
@@ -0,0 +1,3 @@
+@echo off
+java -jar asm1-1.0.jar
+pause
